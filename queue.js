df964e1889f372c31c7320790e5308b161454730
ranji <ranjit.thayyil@gmail.com>
2012-08-26 11:04:09 -0700

    module to read from console and queue

---
+++ queue.js
@@ -0,0 +1,37 @@
+var util = require('util'),
+	events = require('events');
+var queue = [];
+
+function Queue(){
+	if(false === (this instanceof Queue)) {
+        return new Queue();
+    }
+	events.EventEmitter.call(this);
+}
+util.inherits(Queue, events.EventEmitter);
+
+Queue.prototype.enqueue = function(item) {
+	var self = this;
+	self.emit('enqueue', item) 
+	queue.push(item); 
+}
+Queue.prototype.dequeue = function(){
+	var self = this;
+	var item= queue.shift();
+	self.emit('dequeue', item) 
+}
+
+Queue.prototype.startPolling = function(interval){
+	var self = this;
+	setInterval(function(){pollAndDequeue(self);},interval);
+}
+
+function pollAndDequeue(ee){
+		var length= queue.length;
+		ee.emit('polling',length)
+		if (length>0) {
+			var item= queue.shift();
+			ee.emit('dequeue', item)
+			}
+}
+module.exports = Queue;
---
+++ readFromConsole.js
@@ -0,0 +1,41 @@
+var readline = require('readline');
+
+var Queue = require('./queue');
+var queue = new Queue();
+
+queue.on('enqueue',function(item){
+	var statusMessage = 'adding to queue: ' + item
+	console.log(statusMessage);
+})
+
+queue.on('dequeue',function(item){
+	var statusMessage = 'read from queue: ' + item
+	console.log(statusMessage);
+})
+
+queue.on('polling',function(length){
+	if (length>0){
+		var statusMessage = 'items in queue: ' + length
+		console.log(statusMessage);
+	}
+})
+
+queue.startPolling(1000);
+
+
+rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt('ENTER MESSAGE> ');
+rl.prompt();
+
+rl.on('line', function(line) {
+	switch(line.trim()) {
+		default:
+			//raise and event to send message to sqs  
+			queue.enqueue(line);
+			break;
+  }
+	rl.prompt();
+}).on('close', function() {
+  console.log('Have a great day!');
+  process.exit(0);
+});
