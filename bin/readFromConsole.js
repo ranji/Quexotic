d781ef1ed412306577abb4d538807aa5e8cfaac6
ranji <ranjit.thayyil@gmail.com>
2012-08-27 09:28:30 -0700

    added message handler way of messages

--- bin/readFromConsole.js
+++ bin/readFromConsole.js
@@ -1,21 +1,25 @@
 var readline = require('readline');
 var Queue = require('../lib/queue');
+var simpleStringHandler = require('./simpleStringHandler');
 
 var queue = new Queue();
 
-queue.on('enqueue', function (item) {
-	var statusMessage = 'adding to queue: ' + item;
+queue.on('enqueue', function(message) {
+	var statusMessage = 'adding to queue: ' + message;
 	console.log(statusMessage);
 });
-
-queue.on('dequeue', function (item) {
-	var statusMessage = 'read from queue: ' + item;
+/*
+queue.on('dequeue', function(message) {
+	var statusMessage = 'read from queue: ' + message;
 	console.log(statusMessage);
 });
+*/
 
-queue.on('polling', function (length) {
+queue.addMessageHandler(simpleStringHandler);
+
+queue.on('polling', function(length) {
 	if (length > 0) {
-		var statusMessage = 'items in queue: ' + length;
+		var statusMessage = 'messages in queue: ' + length;
 		console.log(statusMessage);
 	}
 });
@@ -27,15 +31,15 @@ rl = readline.createInterface(process.stdin, process.stdout);
 rl.setPrompt('ENTER MESSAGE> ');
 rl.prompt();
 
-rl.on('line', function (line) {
+rl.on('line', function(line) {
 	switch (line.trim()) {
-		default:
-			//raise and event to send message to sqs  
-			queue.enqueue(line);
-			break;
-  }
+	default:
+		//raise and event to send message to sqs  
+		queue.enqueue(line);
+		break;
+	}
 	rl.prompt();
-}).on('close', function () {
-  console.log('Have a great day!');
-  process.exit(0);
+}).on('close', function() {
+	console.log('Have a great day!');
+	process.exit(0);
 });
---
+++ bin/simpleStringHandler.js
@@ -0,0 +1,7 @@
+
+exports.messageType = 'string';
+
+exports.handle = function(message) {
+	console.log('handled string - ' + message)
+}
+
